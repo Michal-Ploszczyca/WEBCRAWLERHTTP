4986b3ce8a78450d042fbce148f599e70401981a
Michal-Ploszczyca <michal.ploszczyca98@gmail.com>
2024-09-13 15:33:41 +0200

    Implemented getURLsFromHTML function to extract and resolve URLs from HTML

--- crawl.js
+++ crawl.js
@@ -1,4 +1,7 @@
 import { urlToHttpOptions } from "url"
+export {normalizeURL};
+import { JSDOM } from 'jsdom'
+export {getURLsFromHTML}
 
 function normalizeURL(fullUrl) {
     const myUrl = new URL(fullUrl)
@@ -10,6 +13,28 @@ function normalizeURL(fullUrl) {
 }
 
 
-export {normalizeURL};
+function getURLsFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody);
+    const anchors = dom.window.document.querySelectorAll('a');
+    const urls = [];
+
+    for (const anchor of anchors) {
+        if (anchor.hasAttribute('href')) {
+            let href = anchor.getAttribute('href')
+
+            try {
+                href = new URL(href, baseURL).href
+                urls.push(href)
+            } catch (error) {
+                console.log(`${error.message}: ${href}`)
+                
+            }
+        }
+    }
+
+    return urls
+
+
+}
 
 
