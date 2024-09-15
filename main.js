77d1036972618d0fc98b1769e3910831a35a3afa
Michal-Ploszczyca <michal.ploszczyca98@gmail.com>
2024-09-15 16:48:26 +0200

    Add main function to recognise correct process.argv.length as a part CLI

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-console.log('hello world')
-// import { urlToHttpOptions } from 'node:url';
+import {argv, exit} from 'node:process';
+import * as readline from 'node:readline'
 
-// const myURL = new URL('http://blog.boot.dev/path/');
 
-// console.log(urlToHttpOptions(myURL));
+function main() {
+    if (process.argv.length < 3) {
+        console.log('No website provided.')
+        exit()
+    };
+    if (process.argv.length > 3) {
+        console.log(`The number of CLI arguments is more than 1`)
+        exit()
+    }
+
+    const baseURL = process.argv[2]
+
+    console.log(`Starting crawl of: ${baseURL}`);
+
+}
+
+
+main()
+
